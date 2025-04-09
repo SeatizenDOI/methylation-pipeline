c533d0f8224fb63ede60d10e81e1c63e94658630
Matis ZOUARI <matis.zouari@insa-lyon.fr>
2025-04-09 13:46:32 +0400

    removed trim galore from bsbolt pipeline

--- bsbolt_pipeline.sh
+++ bsbolt_pipeline.sh
@@ -46,44 +46,24 @@ done
 # Create output directories if they don't exist
 mkdir -p "$OUTPUT_DIR/reports" "$OUTPUT_DIR/trimmed_datasets" "$OUTPUT_DIR/bams"
 
-# If --debug-bam is provided, skip preprocessing steps
-if [[ -n "$DEBUG_BAM" ]]; then
-    echo "Debug mode enabled. Using provided BAM file: $DEBUG_BAM"
-    BASE_NAME=$(basename "$DEBUG_BAM" .bam)
-    cp "$DEBUG_BAM" "$OUTPUT_DIR/${BASE_NAME}.bam"
-    echo "Skipping preprocessing steps. Resuming from BAM processing..."
-else
-    if [ ! -f "$INPUT_FILE" ]; then
-        echo "Error: Input file $INPUT_FILE not found!"
-        exit 1
-    fi
-    echo "Processing file: $INPUT_FILE"
-
-    # Run TrimGalore!
-    trim_galore --rrbs -o "$OUTPUT_DIR" "$INPUT_FILE"
-    # Get the trimmed filename
-    TRIMMED_FILE=$(basename "$INPUT_FILE" .fastq.gz)_trimmed.fq.gz
-
-    # Check if trimming was successful
-    if [ ! -f "$OUTPUT_DIR/$TRIMMED_FILE" ]; then
-        echo "TrimGalore! failed for $INPUT_FILE. Skipping."
-        exit 1
-    fi
-
-    # Extract the unique identifier from the input file
-    BASE_NAME=$(basename "$INPUT_FILE" | sed -E 's/(_QCfiltered)?\.fastq\.gz//')
-    echo "Base name: $BASE_NAME"
-
-    # Name DB for indexing
-    BSBOLT_DB="$GENOME_DIR/BSBOLT_DB"
-    echo "DB name: $BSBOLT_DB"
-
-    # Run BSBolt
-    echo "Running BSBolt alignment on $OUTPUT_DIR/$TRIMMED_FILE using genome from $GENOME_DIR..."
-    python -m bsbolt Align -F1 "$OUTPUT_DIR/$TRIMMED_FILE" -DB $BSBOLT_DB -O "$OUTPUT_DIR/$BASE_NAME"
+if [ ! -f "$INPUT_FILE" ]; then
+    echo "Error: Input file $INPUT_FILE not found!"
+    exit 1
 fi
+echo "Processing file: $INPUT_FILE"
 
-# -------------Start debugging from here--------------
+# Extract the unique identifier from the input file
+BASE_NAME=$(basename "$INPUT_FILE" | sed -E 's/(_QCfiltered)?\.fastq\.gz//')
+echo "Base name: $BASE_NAME"
+
+# Name DB for indexing
+BSBOLT_DB="$GENOME_DIR/BSBOLT_DB"
+echo "DB name: $BSBOLT_DB"
+
+# Run BSBolt
+echo "Running BSBolt alignment on $INPUT_FILE using genome from $GENOME_DIR..."
+python -m bsbolt Align -F1 "$INPUT_FILE" -DB $BSBOLT_DB -O "$OUTPUT_DIR/$BASE_NAME"
+
 # fixmates to prepare for duplicate removal, use -p to disable proper pair check
 echo "samtools fixmate"
 samtools fixmate -p -m "$OUTPUT_DIR/${BASE_NAME}.bam" "$OUTPUT_DIR/${BASE_NAME}.fixmates.bam"
